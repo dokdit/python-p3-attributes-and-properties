e4a11755dcf748dc3241eb1059093ff68d605c0a
Akuei <akueidok911@gmail.com>
2023-07-16 11:13:52 -0400

    Lab almost completed, just have to fix the print statements

--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+
+
+class Person:
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -12,8 +15,33 @@ APPROVED_JOBS = [
     "General Management",
     "Research & Development",
     "Marketing",
-    "Purchasing"
-]
+    "Purchasing"]
 
-class Person:
-    pass
+    def __init__(self,name="",job=""):
+        self.set_name(name)
+        self.set_job(job)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self,name):
+        
+        if isinstance(name, str) and len(name)>0 and len(name)<26:
+            name=name.title()
+            self._name = name
+        else:
+            print( "Name must be string between 1 and 25 characters.")
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self,job):
+        if job in self.APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+
+    name =property(get_name,set_name,)
+    job=property(get_job,set_job,)
+        
